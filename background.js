1caaeb25fe931c95a25e72bdaa492e4a783f3082
Ng Paul Yong <ng.paul.yong@celcom.com.my>
2019-01-15 16:37:44 +0800

    click to go

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 var options = {
   type: "basic",
-  title: "Oi!",
-  message: "Your query has completed!",
+  title: "代替月亮惩罚你!!",
+  message: "动感波波",
   iconUrl: "images/icon.png"
 };
+
+var tabid = 0;
 
 function callBack(){
 
@@ -11,7 +13,22 @@ function callBack(){
 
 chrome.runtime.onMessage.addListener(
     function(response, sender, sendResponse){
-        if (response == 'trigger')
-            chrome.notifications.create(options, callBack);
+        if (response == 'trigger'){
+            tabid = sender.tab.id;
+            chrome.notifications.create("", options, function(notificationId) {
+                setTimeout(function(){
+                    chrome.notifications.clear(notificationId, function(){});
+                }, 10000);
+            })}
+        // else if (response == 'clickedRN'){
+        //     sendResponse(sender);
+        //     chrome.tabs.update(tabid, {active: true});
+        // }
     }
 );
+
+chrome.notifications.onClicked.addListener(function(notificationId, byUser) {
+    // console.log(tabid);
+    chrome.tabs.update(tabid, {active: true});
+    // chrome.tabs.sendMessage(tabid, "callmeback", function(response) {});
+});
--- popup.js
+++ popup.js
@@ -1,24 +1,51 @@
 var map = {}; // You could also use an array
-var isDone = false;
+var isDone = 0;
 
 onkeydown = onkeyup = function(e) {
     e = e || event; // to deal with IE
     map[e.keyCode] = e.type == 'keyup';
     /* insert conditional here */
     if (map[17] && map[13]) { // CTRL+SHIFT+A
-        run();
-        map = {};
+        console.log('ctrlenter',isDone);
+        if(isDone == 0){
+            run();
+            map = {};
+        }
     }
 }
 
 function run(){
     isDone = window.setInterval(checkIfTabPaneActive, 1000);
+    // console.log('isDone',isDone);
 }
 
 function checkIfTabPaneActive(){
-    var result = document.getElementById('queryResults').className;
-    if (result == 'tab-pane active'){
-        chrome.runtime.sendMessage('trigger');
-        window.clearTimeout(isDone);
-    }
+    try{
+		var result = document.getElementById('queryResults').className;
+        // console.log('result ',result);
+        // console.log('isDone inside checktabpaneactive',isDone);
+		if (result == 'tab-pane active'){
+            // console.log('tab-pane active');
+			chrome.runtime.sendMessage('trigger');
+            window.clearTimeout(isDone);
+            isDone = 0;
+		}
+	}
+	catch(TypeError){
+		console.log('Wrong Website');
+
+	}
+
+
 }
+
+// chrome.runtime.onMessage.addListener(
+//     function(respond, sender, sendResponse) {
+//         console.log('content listener');
+//         console.log(respond);
+//         // alert('content listerner');
+//         if (respond == "callmeback") {
+//             chrome.runtime.sendMessage('clickedRN');
+//         }
+//     }
+// );
